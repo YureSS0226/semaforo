da28013140872f286ec451cb1611310adab4cef2
Gabriel Aley <gabrielaley2312@gmail.com>
2025-03-25 09:10:48 -0500

    modificacion de la secuencia del semaforo

--- semaforo/main.py
+++ semaforo/main.py
@@ -3,8 +3,8 @@ from time import sleep
 
 # salidas digitales
 GPIO = const(0x3FF44004)
-vectorPines = [4, 2, 15, 19, 18, 5, 26, 25, 12, 23, 22, 14, 27]
-for i in range(0, 13):
+vectorPines = [4, 2, 15, 19, 18, 5, 26, 25, 12, 23, 22, 14, 27, 16, 17, 21, 3, 1]
+for i in range(0, 18):
     Pin(vectorPines[i], Pin.OUT)
 
 # entradas digitales
@@ -25,7 +25,6 @@ valorTemp = 0.0
 # tiempos
 t1 = 4
 t2 = 2
-t3 = 4
 
 # Interrupciones
 def cambiarModoSemaforo(pin):
@@ -56,53 +55,48 @@ mem32[GPIO] = 0B00000000000000000000000000000000
 while True:
     if bandera == 0:
         if estado == 0:
-            mem32[GPIO] = 0B00000100100000000100000000110000
+            mem32[GPIO] = 0B00001000101010100001000000010000
             sleep(t1)
             for i in range(3):
-                mem32[GPIO] = 0B00000100100000000100000000010000
+                mem32[GPIO] = 0B00001000100010100001000000010000
                 sleep(0.5)
-                mem32[GPIO] = 0B00000100100000000100000000110000
+                mem32[GPIO] = 0B00001000101010100001000000010000
                 sleep(0.5)
-            mem32[GPIO] = 0B00000100100001000100000000010000
+            mem32[GPIO] = 0B00001000100010100001000000011000
             sleep(3)
-            mem32[GPIO] = 0B00001100010010000000000000010000
-            sleep(t2)
+            mem32[GPIO] = 0B00001000100000101001000000100010
+            sleep(t1)
             for i in range(3):
-                mem32[GPIO] = 0B00000100010010000000000000010000
+                mem32[GPIO] = 0B00001000100000101001000000000010
                 sleep(0.5)
-                mem32[GPIO] = 0B00001100010010000000000000010000
+                mem32[GPIO] = 0B00001000100000101001000000100010
                 sleep(0.5)
-            mem32[GPIO] = 0B00000000010010001101000000000000
-            sleep(t3)
+            mem32[GPIO] = 0B00001000100001101001000000000010
+            sleep(3)
+            mem32[GPIO] = 0B00001100100010101000000000000010
+            sleep(t1)
             for i in range(3):
-                mem32[GPIO] = 0B00000000010010000100000000000000
+                mem32[GPIO] = 0B00001000100010100000000000000010
                 sleep(0.5)
-                mem32[GPIO] = 0B00000000010010001101000000000000
+                mem32[GPIO] = 0B00001100100010101000000000000010
                 sleep(0.5)
-            mem32[GPIO] = 0B00000010010010000100000000000100
-            if estado == 0:
+            mem32[GPIO] = 0B00001010100010100000000000000110
+            sleep(3)
+            if estado == 1:
+                print("Modo espera")
+                mem32[GPIO] = 0B00000000010010010101000000010010
+                sleep(t2)
                 for i in range(3):
-                    mem32[GPIO] = 0B00000010000010000100000000000100
+                    mem32[GPIO] = 0B00000000000010000001000000010010
                     sleep(0.5)
-                    mem32[GPIO] = 0B00000010010010000100000000000100
+                    mem32[GPIO] = 0B00000000010010010101000000010010
                     sleep(0.5)
-            else:
-                mem32[GPIO] = 0B00000010010010000100000000000100
-                sleep(3)
-        elif estado == 1:
-            print("Modo espera")
-            mem32[GPIO] = 0B00001100010010000000000000010000
-            sleep(t2)
-            for i in range(3):
-                mem32[GPIO] = 0B00000100000010000000000000010000
-                sleep(0.5)
-                mem32[GPIO] = 0B00001100010010000000000000010000
-                sleep(0.5)
-            estado = 0
-            print("Modo semaforo")
+                estado = 0
+                print("Modo semaforo")
     else:
         mem32[GPIO] = 0B00000000000000000000000000000000
         datoTemp = temp.read()
-        valorTemp = (datoTemp * (5/1023))/0.1 - 4
+        valorTemp = (datoTemp * (5/1023))/0.1
         print(valorTemp)
         sleep(1)
+        
